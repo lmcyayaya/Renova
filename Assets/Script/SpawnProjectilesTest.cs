2c9ab2b4bc61cd03f7709775bcca6d926653f117
Kuei Hua Lee <lmcyayaya@gmail.com>
2019-01-25 22:14:14 +0800

    射擊子彈特效測試

---
+++ Assets/Script/SpawnProjectilesTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SA;
+public class SpawnProjectilesTest : MonoBehaviour
+{
+    
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    public RotateGunTest rotateGunTest;
+
+    private GameObject effectToSpawn;
+    private float timeToFire;
+    void Start()
+    {
+        effectToSpawn = vfx[0]; 
+    }
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1/ effectToSpawn.GetComponent<BulletMove>().fireRate;
+            SpawnBullet();
+        }
+            
+    }
+    public void SpawnBullet()
+    {
+        GameObject vfx;
+        if(firePoint!=null)
+        {
+            vfx = Instantiate(effectToSpawn,firePoint.transform.position,Quaternion.identity);
+            if(rotateGunTest!=null)
+            {
+                vfx.transform.localRotation = rotateGunTest.GetRotation();
+            }
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+
+    }
+}
